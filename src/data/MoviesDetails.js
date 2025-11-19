7c34ad8737d71457b807b3d04e3cfe03ce604bd5
Ayoub EL HERICHI <ayoub.elherichi@gmail.com>
2025-11-19 14:55:43 +0100

    Data created & images

---
+++ src/data/MoviesDetails.js
@@ -0,0 +1,123 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    year: 2010,
+    image: "/images/movies/inception.jpg",
+    description:
+      "A skilled thief who steals secrets through dream-sharing technology is offered a chance at redemption if he can plant an idea in someone's mind.",
+    team: [
+      { name: "Leonardo DiCaprio", photo: "/images/team/leonardo_dicaprio.jpg" },
+      { name: "Joseph Gordon-Levitt", photo: "/images/team/joseph_gordon_levitt.jpg" },
+      { name: "Elliot Page", photo: "/images/team/elliot_page.jpg" },
+      { name: "Christopher Nolan (Director)", photo: "/images/team/christopher_nolan.jpg" }
+    ]
+  },
+
+  {
+    id: 2,
+    title: "Interstellar",
+    year: 2014,
+    image: "/images/movies/interstellar.jpg",
+    description:
+      "A team of explorers travels beyond our galaxy through a wormhole to find a new planet capable of sustaining human life.",
+    team: [
+      { name: "Matthew McConaughey", photo: "/images/team/matthew_mcconaughey.jpg" },
+      { name: "Anne Hathaway", photo: "/images/team/anne_hathaway.jpg" },
+      { name: "Jessica Chastain", photo: "/images/team/jessica_chastain.jpg" },
+      { name: "Christopher Nolan (Director)", photo: "/images/team/christopher_nolan.jpg" }
+    ]
+  },
+
+  {
+    id: 3,
+    title: "The Dark Knight",
+    year: 2008,
+    image: "/images/movies/dark_knight.jpg",
+    description:
+      "Batman battles the Joker, a criminal mastermind who brings chaos to Gotham City.",
+    team: [
+      { name: "Christian Bale", photo: "/images/team/christian_bale.jpg" },
+      { name: "Heath Ledger", photo: "/images/team/heath_ledger.jpg" },
+      { name: "Gary Oldman", photo: "/images/team/gary_oldman.jpg" },
+      { name: "Christopher Nolan (Director)", photo: "/images/team/christopher_nolan.jpg" }
+    ]
+  },
+
+  {
+    id: 4,
+    title: "Avatar",
+    year: 2009,
+    image: "/images/movies/avatar.jpg",
+    description:
+      "A marine dispatched to the moon Pandora becomes torn between following orders and protecting the world he now calls home.",
+    team: [
+      { name: "Sam Worthington", photo: "/images/team/sam_worthington.jpg" },
+      { name: "Zoe Saldana", photo: "/images/team/zoe_saldana.jpg" },
+      { name: "Sigourney Weaver", photo: "/images/team/sigourney_weaver.jpg" },
+      { name: "James Cameron (Director)", photo: "/images/team/james_cameron.jpg" }
+    ]
+  },
+
+  {
+    id: 5,
+    title: "Titanic",
+    year: 1997,
+    image: "/images/movies/titanic.jpg",
+    description:
+      "A young aristocrat falls in love with a poor artist aboard the luxurious but doomed RMS Titanic.",
+    team: [
+      { name: "Leonardo DiCaprio", photo: "/images/team/leonardo_dicaprio.jpg" },
+      { name: "Kate Winslet", photo: "/images/team/kate_winslet.jpg" },
+      { name: "Billy Zane", photo: "/images/team/billy_zane.jpg" },
+      { name: "James Cameron (Director)", photo: "/images/team/james_cameron.jpg" }
+    ]
+  },
+
+  {
+    id: 6,
+    title: "The Matrix",
+    year: 1999,
+    image: "/images/movies/matrix.jpg",
+    description:
+      "A hacker discovers that reality is a simulation and joins a rebellion to free humanity from machine control.",
+    team: [
+      { name: "Keanu Reeves", photo: "/images/team/keanu_reeves.jpg" },
+      { name: "Carrie-Anne Moss", photo: "/images/team/carrie_anne_moss.jpg" },
+      { name: "Laurence Fishburne", photo: "/images/team/laurence_fishburne.jpg" },
+      { name: "Lana Wachowski (Director)", photo: "/images/team/wachowskis.jpg" }
+    ]
+  },
+
+  {
+    id: 7,
+    title: "Spider-Man: Into the Spider-Verse",
+    year: 2018,
+    image: "/images/movies/spiderverse.jpg",
+    description:
+      "Teenager Miles Morales becomes the new Spider-Man and meets alternate Spider-People across the multiverse.",
+    team: [
+      { name: "Shameik Moore", photo: "/images/team/shameik_moore.jpg" },
+      { name: "Jake Johnson", photo: "/images/team/jake_johnson.jpg" },
+      { name: "Hailee Steinfeld", photo: "/images/team/hailee_steinfeld.jpg" },
+      { name: "Peter Ramsey (Director)", photo: "/images/team/peter_ramsey.jpg" }
+    ]
+  },
+
+  {
+    id: 8,
+    title: "La La Land",
+    year: 2016,
+    image: "/images/movies/lalaland.jpg",
+    description:
+      "A jazz musician and an aspiring actress pursue their dreams in Los Angeles while trying to maintain their relationship.",
+    team: [
+      { name: "Ryan Gosling", photo: "/images/team/ryan_gosling.jpg" },
+      { name: "Emma Stone", photo: "/images/team/emma_stone.jpg" },
+      { name: "John Legend", photo: "/images/team/john_legend.jpg" },
+      { name: "Damien Chazelle (Director)", photo: "/images/team/damien_chazelle.jpg" }
+    ]
+  }
+];
+
+export default movies;
