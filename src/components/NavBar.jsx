a7c7a723a63e590b9c6868abff5247bd5a10afb2
BLVK-MAN <ounajem71@gmail.com>
2025-11-21 17:01:50 +0100

    Home page,navbar et footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <header className="navbar">
+      <div className="Nav_cine">
+        <h1>Cineverse</h1>
+      </div>
+      <nav className="nav_links">
+        <NavLink to="/">
+          Home
+        </NavLink>
+        <NavLink to="/Contact" >
+          Contact us
+        </NavLink>
+      </nav>
+    </header>
   )
 }
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,16 @@
+import "../App.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2025 Cineverse. All rights reserved.</p>
+        
+        <div className="footer-links">
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms of Use</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
